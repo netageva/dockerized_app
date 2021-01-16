625b62c7b9c115c0c16fa438b17832970a47c721
Neta Geva <ngeva91@gmail.com>
2021-01-16 12:42:22 +0200

    fixing app.run

--- inference.py
+++ inference.py
@@ -52,5 +52,4 @@ def multiple_predictions():
 
     return y_pred_json
 if __name__ == '__main__':
-    port = os.environ.get('PORT')
-    app.run(host='0.0.0.0', port=int(port), debug=True)
+    app.run(host='0.0.0.0')
